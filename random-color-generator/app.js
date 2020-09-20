552b656614744769f3370566efaa74c625cfe758
vishwajeet singh chauhan <vishwajeet11104736@gmail.com>
2020-09-20 20:46:08 +0530

    first commit

---
+++ random-color-generator/app.js
@@ -0,0 +1,11 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+btn.addEventListener('click', ()=> {
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+const getRandomNumber = () => Math.floor(Math.random()*colors.length);
